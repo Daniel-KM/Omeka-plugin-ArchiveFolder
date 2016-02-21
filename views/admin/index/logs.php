ced6341805219426875e26ed8c623e3d54143da9
Daniel Berthereau <Daniel.github@Berthereau.net>
2016-02-22 00:00:00 +0100

    Fixed logs view for multilined messages.

--- views/admin/index/logs.php
+++ views/admin/index/logs.php
@@ -10,7 +10,9 @@
 
         $messages = $archiveFolder->messages;
         if (!empty($messages)):
-            $messages = explode(PHP_EOL, $messages);
+            $dateTimePattern = '(^\[[1-2][0-9]{3}-(?:0[1-9]|1[0-2])-(?:0[1-9]|[1-2][0-9]|3[0-1]) (?:[0-1][0-9]|2[0-3]):(?:[0-5][0-9]):(?:[0-5][0-9])\]) ';
+            $priorityPattern = '(.*?): ';
+            $messages = preg_split('/' . $dateTimePattern . $priorityPattern . '/m', $messages, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);
             ?>
         <table class="simple">
             <thead>
@@ -21,28 +23,22 @@
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($messages as $message): ?>
+                <?php foreach ($messages as $i =>$message):
+                    if ($i % 3) continue;
+                    $date = substr($message, 1, 10);
+                    $time = substr($message, 12, 8);
+                    $priority = $messages[$i + 1];
+                    $message = trim($messages[$i + 2]);
+                ?>
                 <tr>
-                    <td>
-                        <?php
-                        $date = substr($message, 1, 10);
-                        $time = substr($message, 12, 8);
+                    <td><?php
                         echo html_escape(format_date($date, Zend_Date::DATE_SHORT));
-                        echo PHP_EOL;
+                        echo ' ';
                         echo html_escape(format_date($time, Zend_Date::TIME_MEDIUM));
-                        ?>
+                    ?></td>
+                    <td><?php echo $priority; ?>
                     </td>
-                    <td>
-                        <?php
-                            $colon = strpos($message, ':', 22);
-                            $priority = substr($message, 22, $colon - 22);
-                            echo $priority;
-                        ?>
-                    </td>
-                    <td>
-                        <?php
-                            echo html_escape(substr($message, $colon + 2));
-                        ?>
+                    <td><?php echo html_escape($message); ?>
                     </td>
                 </tr>
                 <?php endforeach; ?>
